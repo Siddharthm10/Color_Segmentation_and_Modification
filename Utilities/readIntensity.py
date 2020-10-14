e833b9cb8babf3c0a462bc6b44b16dff63457b9e
siddharthm10 <mehtasid130499@gmail.com>
2020-10-14 14:36:54 +0530

    reverted

--- Utilities/readIntensity.py
+++ Utilities/readIntensity.py
@@ -8,7 +8,7 @@ upper = np.array([159,255,255])
 imgHSV = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
 mask = cv2.inRange(image,lower,upper)
 gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-temp = (np.multiply(2,cv2.split(gray_image)))
+temp = (np.multiply(2.4,cv2.split(gray_image)))
 
 b,g,r = temp[0],temp[0],temp[0]
 R = np.multiply(1,r)
